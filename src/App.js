766865c7867bb5a937724ddd223ecaf491c77314
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-06-18 18:37:27 +0300

    feat: add error page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SharedLayout from "./components/SharedLayout/SharedLayout";
 import HomePage from "./pages/HomePage/HomePage";
 import CatalogPage from "./pages/CatalogPage/CatalogPage";
 import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="catalog" element={<CatalogPage />} />
         <Route path="favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   );
