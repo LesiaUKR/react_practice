a24425d657701857018f50d4c5ec8ccb4fe3d591
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-06-26 23:33:33 +0300

    feat: add categories array

--- src/components/CategoryBadge/CategoryBadge.jsx
+++ src/components/CategoryBadge/CategoryBadge.jsx
@@ -2,13 +2,15 @@ import React, { Fragment } from "react";
 import Icon from "../Icon/Icon";
 import { CategoryNameWrapper, IconWrapper } from "./CategoryBadge.styled";
 
-const CategoryBadge = ({ icon, feature }) => {
+const CategoryBadge = ({ icon, feature, featureQuantity }) => {
   return (
     <Fragment>
       <IconWrapper>
         <Icon name={icon} width="20" height="20" />
       </IconWrapper>
-      <CategoryNameWrapper>2 adults</CategoryNameWrapper>
+      <CategoryNameWrapper>
+        {featureQuantity} {feature}
+      </CategoryNameWrapper>
     </Fragment>
   );
 };
--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -4,9 +4,33 @@ import CategoryBadge from "../CategoryBadge/CategoryBadge";
 import Icon from "../Icon/Icon";
 
 const ItemCard = ({ advert }) => {
-  const { name, price, rating, location, description, gallery } = advert;
-  console.log(advert);
+  const {
+    name,
+    price,
+    rating,
+    location,
+    adults,
+    engine,
+    transmission,
+    description,
+    gallery,
+    details,
+    reviews,
+  } = advert;
 
+  const categories = [
+    { icon: "adults", text: `${adults} adults` },
+    { icon: "transmission", text: `${transmission}` },
+  ];
+
+  // adults,
+  // [transmission]: transmission,
+  // engine: capitalizeFirstLetter(engine),
+  // Kitchen: details.kitchen,
+  // beds: details.beds,
+  // AC: details.airConditioner,
+
+  console.log(categories);
   let formattedLocation = "";
   if (location) {
     let [country, city] = location.split(", ");
@@ -35,10 +59,22 @@ const ItemCard = ({ advert }) => {
         <div>
           <Icon name="star" />
           <p>{rating}</p>
+          <span>({reviews.length} Reviews)</span>
+        </div>
+        <div>
+          <p>{description}</p>
+          <Icon name="location" />
           <p>{formattedLocation}</p>
         </div>
+        <ul>
+          {categories.map((category) => (
+            <li key={category.icon}>
+              <CategoryBadge icon={category.icon} feature={category.text} />
+            </li>
+          ))}
+        </ul>
+
         <DefaultBtn text="Show more" />
-        <CategoryBadge icon="water" />
       </div>
     </div>
   );
